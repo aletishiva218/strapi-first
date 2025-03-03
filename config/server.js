a37f792e4fd744b3bb0f70428e9180a2879440b6
Shiva Aleti <aletishiva218@gmail.com>
2025-03-04 01:08:58 +0530

    installed pg

--- config/server.js
+++ config/server.js
@@ -16,6 +16,18 @@ module.exports = ({ env }) => ({
   app: {
     keys: env.array("APP_KEYS"),
   },
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: { rejectUnauthorized: false }, // Ensures SSL connection
+    },
+    pool: { min: 0, max: 10 },
+  },
     webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
