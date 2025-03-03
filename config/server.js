2df3a3431bed7f59dce7e73c58b5a6fab66e6e89
Shiva Aleti <aletishiva218@gmail.com>
2025-03-04 00:46:30 +0530

    updated to develop mode

--- config/server.js
+++ config/server.js
@@ -1,10 +1,22 @@
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 8000),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+//   webhooks: {
+//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+//   },
+// });
+
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8000),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 8000),
+  url: env("PUBLIC_URL", "https://respective-gianina-shivatech-solutions-c76c4053.koyeb.app/"),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
-  webhooks: {
+    webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
