c54f5c1c2d4dd9768ea8a5b42b6f72492056ed16
Shiva Aleti <aletishiva218@gmail.com>
2025-03-04 06:17:38 +0530

    updated node.js version

--- config/database.js
+++ config/database.js
@@ -11,7 +11,7 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: { rejectUnauthorized: false },
+        ssl: { rejectUnauthorized: false }
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
